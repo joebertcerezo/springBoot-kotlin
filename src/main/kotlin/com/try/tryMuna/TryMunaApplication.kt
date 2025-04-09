11b1841cce9e7213576ee993484f62ca22f0e366
sirezo <joebertcerezo@gmail.com>
2025-04-09 10:51:03 +0800

    Create TryMunaApplication.kt

---
+++ src/main/kotlin/com/try/tryMuna/TryMunaApplication.kt
@@ -0,0 +1,11 @@
+package com.`try`.tryMuna
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TryMunaApplication
+
+fun main(args: Array<String>) {
+	runApplication<TryMunaApplication>(*args)
+}
