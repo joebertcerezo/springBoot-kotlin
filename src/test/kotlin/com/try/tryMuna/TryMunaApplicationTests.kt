54f4026c0a037291ed3df996fd2f68328c7a870c
sirezo <joebertcerezo@gmail.com>
2025-04-09 10:51:10 +0800

    Create TryMunaApplicationTests.kt

---
+++ src/test/kotlin/com/try/tryMuna/TryMunaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.`try`.tryMuna
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TryMunaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
