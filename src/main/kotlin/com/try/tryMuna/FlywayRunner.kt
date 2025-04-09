6a037f4eec731c0f264021debc94328fe0296d2d
sirezo <joebertcerezo@gmail.com>
2025-04-09 16:45:58 +0800

    Create FlywayRunner.kt

---
+++ src/main/kotlin/com/try/tryMuna/FlywayRunner.kt
@@ -0,0 +1,12 @@
+// package com.`try`.tryMuna
+
+// import org.flywaydb.core.Flyway
+// import org.springframework.boot.CommandLineRunner
+// import org.springframework.stereotype.Component
+
+// @Component
+// class FlywayRunner(private val flyway: Flyway) : CommandLineRunner {
+//   override fun run(vararg args: String?) {
+//     flyway.migrate()
+//   }
+// }
