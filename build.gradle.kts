72096be5bcfa3875f37c264a3dd8037e7493152b
sirezo <joebertcerezo@gmail.com>
2025-04-09 16:45:56 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 	kotlin("plugin.jpa") version "1.9.25"
 	id("org.springframework.boot") version "3.4.4"
 	id("io.spring.dependency-management") version "1.1.7"
+	// id("org.flywaydb.flyway") version "10.0.0"
 }
 
 group = "com.try"
@@ -27,6 +28,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.postgresql:postgresql")
+	implementation("org.flywaydb:flyway-database-postgresql")
 
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -51,3 +53,10 @@ tasks.withType<KotlinCompile> {
     freeCompilerArgs += "-Xjsr305=strict"
   }
 }
+
+// flyway {
+//     url = System.getenv("DB_URL") ?: "jdbc:postgresql://localhost:5432/spring_kot"
+//     user = System.getenv("DB_USER") ?: "jlcerezo"
+//     password = System.getenv("DB_PASS") ?: ""
+//     locations = arrayOf("classpath:/db/migration")
+// }
