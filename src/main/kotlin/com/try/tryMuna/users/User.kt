a2fa42c376e20b10e56ad188dd9d1bb6ca1c1dc2
sirezo <joebertcerezo@gmail.com>
2025-04-10 06:45:53 +0800

    Update User.kt

--- src/main/kotlin/com/try/tryMuna/users/User.kt
+++ src/main/kotlin/com/try/tryMuna/users/User.kt
@@ -3,9 +3,12 @@ package com.`try`.tryMuna.users
 import jakarta.persistence.*
 
 @Entity
-@Table(name = "USERS")
+@Table(name = "\"USERS\"")
 data class User(
-        @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long,
+        @Id 
+        @GeneratedValue(strategy = GenerationType.IDENTITY) 
+        val id: Integer,
+        @Column(nullable = false)
         val name: String,
         val email: String
 )
