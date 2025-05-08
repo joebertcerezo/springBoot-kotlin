046fed2cf198f83c12cb911f5e1ab8c31f76e0a7
sirezo <joebertcerezo@gmail.com>
2025-05-08 16:29:39 +0800

    Update UserRepo.kt

--- src/main/kotlin/com/try/tryMuna/users/UserRepo.kt
+++ src/main/kotlin/com/try/tryMuna/users/UserRepo.kt
@@ -1,1 +1,7 @@
 package com.`try`.tryMuna.users
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepo : JpaRepository<User, Long> {
+  fun findByEmail(email: String): User
+}
