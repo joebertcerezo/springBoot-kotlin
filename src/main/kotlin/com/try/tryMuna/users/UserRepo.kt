832d3bb2d21115ad371ae091c56e3db396c56c9d
sirezo <joebertcerezo@gmail.com>
2025-05-09 13:44:18 +0800

    Delete UserRepo.kt

--- src/main/kotlin/com/try/tryMuna/users/UserRepo.kt
+++
@@ -1,7 +0,0 @@
-package com.`try`.tryMuna.users
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserRepo : JpaRepository<User, Long> {
-  fun findByEmail(email: String): User
-}
