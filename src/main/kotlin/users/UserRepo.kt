842f8fd70bea988e3a46d95a4c34a481b97746db
sirezo <joebertcerezo@gmail.com>
2025-05-09 15:54:28 +0800

    Update UserRepo.kt

--- src/main/kotlin/users/UserRepo.kt
+++ src/main/kotlin/users/UserRepo.kt
@@ -1,9 +1,12 @@
 package users
 
+import java.util.UUID
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserRepo : JpaRepository<User, Long> {
+interface UserRepo : JpaRepository<User, UUID>, UserCustomRepository {
   fun findByEmail(email: String): User
+  fun findAllByEmail(email: String): List<User>
 }
